402ec78f3a6901029bc09028647f8dbeda883302
GustavoMends <gusttavo.me@outlook.com>
2022-02-12 21:38:46 -0300

    sm8250-common: Drop remove_packages

--- kona.mk
+++ kona.mk
@@ -474,10 +474,6 @@ PRODUCT_PACKAGES += \
     libvndfwk_detect_jni.qti \
     libvndfwk_detect_jni.qti.vendor
     
-# RemovePackages
-PRODUCT_PACKAGES += \
-    RemovePackages
-
 # RIL
 PRODUCT_PACKAGES += \
     libprotobuf-cpp-full \
